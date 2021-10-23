19a825f2d44a883f4bcdfabf05bb1094c7349084
stephenisau <sfcstau@gmail.com>
2021-10-22 17:23:14 -0700

    modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root "todos#index"
   
-  resources :todos 
+  get "/todos/:month" => "todos#get_by_month"
+  get "/todos/:date" => "todos#get_by_date"
+  # RESTful routes for all our todos
+  resources :todos
 
   delete "/todos/:id" => "todos#destroy"
 end
